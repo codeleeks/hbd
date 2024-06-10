d275d7e0345ebf102d77e1146005406bb479cae5
codeleeks <kasonglee@gmail.com>
2024-06-10 17:54:39 +0900

    home에 하트 애니메이션 추가

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,14 @@ interface HomeProps {}
 const Home = (_: HomeProps) => {
   return (
     <div className='heart inner'>
-      <img src={heart} alt='1년 동안 희주가 행복했던 이미지!' />
+      <ul className='canvas'>
+        {[...Array(50)].map((_, i) => (
+          <li key={i} className='star'></li>
+        ))}
+      </ul>
+      <div className='heart__image'>
+        <img src={heart} alt='1년 동안 희주가 행복했던 이미지!' />
+      </div>
     </div>
   )
 }
